bb2416f47ad487972707129d6390b7fec77fa659
robertarnesson <robert.arnesson@gmail.com>
2018-02-28 20:42:58 +0100

    build script fix

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+set build_dir=%~dp0
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+cd %build_dir%
 msbuild build\xrig.sln /p:Configuration=MinSizeRel
