199a18387c0ebbd2ceeec3f764a7b87fce173359
robertarnesson <robert.arnesson@gmail.com>
2018-03-01 20:16:34 +0100

    bat file fixes

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
-set build_dir=%~dp0
+set project_dir=%~dp0
 set configuration=MinSizeRel
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-cd %build_dir%
+cd %project_dir%
 msbuild build\xrig.sln /p:Configuration=%configuration%
 xcopy libuv-1.14.1-x64\libuv.dll build\%configuration%\ /Y
 xcopy devcon\devcon.exe build\%configuration%\ /Y
--- generate_sln.bat
+++ generate_sln.bat
@@ -1,4 +1,6 @@
+set project_dir=%~dp0
 rd /s /q "build"
 mkdir build
 cd build
 cmake .. -G "Visual Studio 15 2017 Win64" -DUV_INCLUDE_DIR="..\libuv-1.14.1-x64\include" -DUV_LIBRARY="..\libuv-1.14.1-x64\libuv.lib" -DMHD_INCLUDE_DIR="..\libmicrohttpd-0.9.58-w32-bin\x86_64\VS2017\Release-static" -DMHD_LIBRARY="..\libmicrohttpd-0.9.58-w32-bin\x86_64\VS2017\Release-static\libmicrohttpd.lib"
+cd %project_dir%
