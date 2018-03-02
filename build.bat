27d35c2b9ea1eb858d8ea633008587890ac43bcc
robertarnesson <robert.arnesson@gmail.com>
2018-03-02 16:52:55 +0100

    bat fix

--- build.bat
+++ build.bat
@@ -6,8 +6,8 @@ msbuild build\xrig.sln /p:Configuration=%configuration%
 xcopy libuv-1.14.1-x64\libuv.dll build\%configuration%\ /Y
 xcopy devcon\devcon.exe build\%configuration%\ /Y
 (
-echo cd %%~dp0
 echo @echo off
+echo cd %%~dp0
 echo set yyyy=%%date:~10,4%%
 echo set mm=%%date:~4,2%%
 echo set dd=%%date:~7,2%%
@@ -19,12 +19,12 @@ echo xrig.exe --colors --log=xrig_%%yyyy%%%%mm%%%%dd%%_%%hh%%%%mm%%%%ss%%.log
 echo pause
 ) > build\%configuration%\run.bat
 (
+echo @echo off
 echo cd %%~dp0
 echo devcon.exe disable "PCI\VEN_1002&DEV_687F"
 echo timeout /t 5
 echo devcon.exe enable "PCI\VEN_1002&DEV_687F"
 echo timeout /t 5
-echo @echo off
 echo set yyyy=%%date:~10,4%%
 echo set mm=%%date:~4,2%%
 echo set dd=%%date:~7,2%%
